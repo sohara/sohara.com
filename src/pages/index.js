db7f6f23b9ff0ed00a7386f2692af0e7acbff26e
Sean O'Hara <sohara@sohara.com>
2019-01-13 13:15:28 +1100

    Update index with selfie and blurb

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
+import selfie from "../images/IMG_2590.jpg";
 
 const IndexPage = () => (
   <Layout>
@@ -12,25 +12,13 @@ const IndexPage = () => (
     />
 
     <div className="text-center">
-      <img
-        src={catAndHumanIllustration}
-        className="block mx-auto w-1/2"
-        alt="Cat and human sitting on a couch"
-      />
+      <img src={selfie} className="block mx-auto w-3/4" alt="Sean O'Hara" />
 
-      <h2 className="bg-yellow inline-block my-8 p-3">
-        Hey there! Welcome to your first Gatsby site.
-      </h2>
+      <h2 className="inline-block my-8 p-3">Sean O'Hara</h2>
 
       <p className="leading-loose">
-        This is a barebones starter for Gatsby styled using{" "}
-        <a
-          href="https://tailwindcss.com/"
-          className="font-bold no-underline text-grey-darkest"
-        >
-          Tailwind
-        </a>
-        , a utility-first CSS framework.
+        I'm a web developer with over a decade of experience now specializing in
+        single page applications.
       </p>
     </div>
   </Layout>
